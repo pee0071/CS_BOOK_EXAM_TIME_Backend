5821f68f9cf968b05d1770bc16b7fed467147bef
raysupakit138 <supakit.suks@ku.th>
2024-01-04 16:44:15 +0700

    sign up backend

--- book_exam_backend/book_exam_backend/settings.py
+++ book_exam_backend/book_exam_backend/settings.py
@@ -31,11 +31,11 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 # Application definition
 AUTH_USER_MODEL = "account.User"
